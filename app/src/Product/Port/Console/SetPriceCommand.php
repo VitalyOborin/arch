f14b155c52e9cf405c7305ec78cd7d21dae70fbe
Vitaly Oborin <vitaminych@gmail.com>
2023-04-20 22:52:59 +0300

    Удален make. Добавлен psalm. Исправлен docker-compose. Добавлен rsync в app для синхронизации локальной разработки под windows

---
+++ app/src/Product/Port/Console/SetPriceCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\Product\Port\Console;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:set-price',
+    description: 'Set new price for product',
+)]
+class SetPriceCommand extends Command
+{
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('alias', InputArgument::REQUIRED, 'Product alias')
+            ->addArgument('price', InputArgument::REQUIRED, 'Product price')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $alias = (string) $input->getArgument('alias');
+        $price = (int) $input->getArgument('price');
+
+        try {
+            $io->comment('make some magic...');
+        } catch (\Exception $e) {
+            $io->error($e->getMessage());
+
+            return Command::FAILURE;
+        }
+
+        $io->success(sprintf('Product with code = %s has been updated, new price = %d', $alias, $price));
+
+        return Command::SUCCESS;
+    }
+}
